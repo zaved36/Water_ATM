af98ac43e75162cbb7e7ef238aa362bd0ad3e682
Your Name <you@example.com>
2022-06-22 22:47:05 +0600

    first commit

---
+++ stores.js
@@ -0,0 +1,351 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3710782999999, 23.7895761]
+        },
+        "properties": {
+          "name": "Shewrapara 02 ",
+          "address": "Dhaka WASA ATM Booth, Zone 04",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.428853, 23.7266029]
+        },
+        "properties": {
+          "name": "Mugdapara 02",
+          "address": "Dhaka WASA ATM Booth, ",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4109681,23.7862578]
+        },
+        "properties": {
+          "name": "Siddeshwari Boys",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.403136, 23.7862578]
+        },
+        "properties": {
+          "name": "Banani 03",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3814100999999, 23.7296255999999]
+        },
+        "properties": {
+          "name": "Iraqi Kabarstan",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4016366999999, 23.7781096]
+        },
+        "properties": {
+          "name": "Mohakhali Laboratory",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4311435999999,23.7374164999999]
+        },
+        "properties": {
+          "name": "Kadamtala",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3843497999999, 23.7986885999999]
+        },
+        "properties": {
+          "name": "Ibrahimpur ",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4023729, 23.7605949]
+        },
+        "properties": {
+          "name": "Dhaka Polytechnic",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4076586, 23.7181788]
+        },
+        "properties": {
+          "name": "Nazirabazar",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3995793999999, 23.7753109]
+        },
+        "properties": {
+          "name": "Mohakhali Dakkhinpara",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4331231, 23.7289995]
+        },
+        "properties": {
+          "name": "Mugda 01",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4523705999999, 23.712871]
+        },
+        "properties": {
+          "name": "Matuail Mridhabari ",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4348116,23.7414025]
+        },
+        "properties": {
+          "name": "Basabo 02 ",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4419711, 23.7442099999999]
+        },
+        "properties": {
+          "name": "Madartek Old Dhaka",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3681171, 23.8232399999999]
+        },
+        "properties": {
+          "name": "Mirpur 12/B",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3749483999999, 23.8297752]
+        },
+        "properties": {
+          "name": "Mirpur 12/D",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4007541999999,23.751185]
+        },
+        "properties": {
+          "name": "Dilu Road",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4244635999999, 23.7468617]
+        },
+        "properties": {
+          "name": "Khilgaon Bagicha",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3973826, 23.8777751999999]
+        },
+        "properties": {
+          "name": "Uttara 9/A",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4177849999999, 23.8686452]
+        },
+        "properties": {
+          "name": "Chalabon",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4377123, 23.7458992]
+        },
+        "properties": {
+          "name": "Goran 11 ",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4324814999999, 23.7492085]
+        },
+        "properties": {
+          "name": "Goran Nurbag",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4123761999999, 23.7585278]
+        },
+        "properties": {
+          "name": "Modhubag Old",
+          "address": "an Nurbag",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4238035, 23.7559903]
+        },
+        "properties": {
+          "name": "Mouluvirtek",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4578242999999, 23.6975444]
+        },
+        "properties": {
+          "name": "Rayerbag",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.3774485, 23.8228228999999]
+        },
+        "properties": {
+          "name": "Mirpur 12/E",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4311684, 23.7632423999999]
+        },
+        "properties": {
+          "name": "Banasree 01",
+          "address": "Dhaka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [90.4319476, 23.7467222999999]
+        },
+        "properties": {
+          "name": "Goran Bhuiyar Jheel",
+          "address": "haka WASA ATM Booth",
+          "phone": "23 2323 2323"
+        }
+      }
+]
